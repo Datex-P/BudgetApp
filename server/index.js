5ed19f1352db16ac88092344cee992117cc59bc2
Валерій Зінченко <mac@MacBook-Air-Valerij.local>
2020-10-12 12:07:51 +0300

    added post request

--- server/index.js
+++ server/index.js
@@ -1,32 +1,45 @@
 var express = require('express')
 var app = express();
 
-let users = {};
+app.use(express.json())
 
-app.get('/:occupation/:city/', (req,res)=>{
-  // console.log(req.params)
-  // console.log(req.query)
-  // mongo.find({...req.params,...req.query})
-  let {occupation,city} = req.params
+
+let users = {};
+app.get('/:param',(req,res)=>{
+  res.send('Hi, nice to meet your on our resorse '+req.params.param)
+})
+app.post('/', (req,res)=>{
+
+  let {occupation,city,...other} = req.body
   if(!(occupation in users)){
     users[occupation] = {}
   }
   if(!(city in users[occupation])){
     users[occupation][city] = []
   }
-  users[occupation][city].push(req.query)
+  users[occupation][city].push(other)
   
+  console.log(JSON.stringify(users, null, 3))
+  res.send('ok')
+})
+app.get('/:occupation/:city/', (req,res)=>{
+  let {occupation,city} = req.params
+  let answer = null
+  if(users[occupation] && users[occupation][city]){
+    answer = users[occupation][city]
+  }
+
 
   res.send(`Hi you visited: 
-  <ul>
-    <li>protocol: ${req.protocol}</li>
-    <li>host: ${req.get('host')}</li>
-    <li>url: ${req.originalUrl}</li>
-  </ul>
-  ${JSON.stringify(users)}
+    <ul>
+      <li>protocol: ${req.protocol}</li>
+      <li>host: ${req.get('host')}</li>
+      <li>url: ${req.originalUrl}</li>
+    </ul>
+    ${JSON.stringify(answer)}
   `)
 })
 
-// app.post()
+
 
 app.listen(3333, ()=>console.log('hello'))
